972b0268cc1c433fe00b60f5ba20990479cad0de
jcmichelou <jean-claude.michelou@spinetix.com>
2020-04-03 03:21:04 +0200

    BUILD: Adapt build instructions for branch chrome/3987

--- public/fpdfview.h
+++ public/fpdfview.h
@@ -167,7 +167,7 @@ typedef int FPDF_OBJECT_TYPE;
 // Text object enums.
 typedef int FPDF_TEXT_RENDERMODE;
 
-#if defined(COMPONENT_BUILD)
+#if 1
 // FPDF_EXPORT should be consistent with |export| in the pdfium_fuzzer
 // template in testing/fuzzers/BUILD.gn.
 #if defined(WIN32)
